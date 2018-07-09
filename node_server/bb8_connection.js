f9fb65bc8860a7706d2668434290b000258d5807
unknown <remi.pachet@etu.u-bordeaux.fr>
2018-07-09 11:54:28 +0200

    Test color

--- node_server/bb8_connection.js
+++ node_server/bb8_connection.js
@@ -2,8 +2,10 @@ var sphero = require("sphero"),
 bb8 = sphero("DD:3F:27:D5:69:EE");
 
 bb8.connect(function(){
-  setInterval(function(){
-    var direction = Math.floor(Math.random() * 360);
-    bb8.roll(150, direction);
-  }, 1000);
+  setTimeout(function(){
+    // var direction = Math.floor(Math.random() * 360);
+    // bb8.roll(150, direction);
+    orb.color("blue");
+  }, 100);
+
 })
