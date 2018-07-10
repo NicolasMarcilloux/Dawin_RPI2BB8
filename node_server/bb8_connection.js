e20378b197abdc8806c86896283b7a74ff07fd0a
unknown <remi.pachet@etu.u-bordeaux.fr>
2018-07-10 09:37:34 +0200

    Add function bb8Action

--- node_server/bb8_connection.js
+++ node_server/bb8_connection.js
@@ -1,11 +1,42 @@
 var sphero = require("sphero"),
 bb8 = sphero("DD:3F:27:D5:69:EE");
 
-bb8.connect(function(){
-  setTimeout(function(){
-    // var direction = Math.floor(Math.random() * 360);
-    // bb8.roll(150, direction);
-    orb.color("blue");
-  }, 100);
 
+
+function bb8Action(action){
+
+  switch (action) {
+    case "MoveUp":
+    setInterval(function() {
+      bb8.roll(150, 0);
+    }, 1000);
+      break;
+
+    case "MoveDown":
+
+      break;
+
+    case "MoveRight":
+
+      break;
+
+    case "MoveLeft":
+
+      break;
+
+    case "Color":
+
+      break;
+
+    case "Setup":
+
+      break;
+
+  }
+
+}
+
+bb8.connect(function(){
+  bb8.detectCollisions();
+  bb8Action("MoveUp");
 })
